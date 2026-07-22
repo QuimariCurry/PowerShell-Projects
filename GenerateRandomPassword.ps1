175dd9c9219bca4e7aeac1b416d2fb840d0150a7
QuimariCurry <quimari.curry@gmail.com>
2026-07-22 19:48:31 -0400

    Delete GenerateRandomPassword.ps1

--- GenerateRandomPassword.ps1
+++
@@ -1,4 +0,0 @@
-﻿$length = 14
-$character = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()'
-$random = -join ((Get-Random -Count $length -InputObject $character.ToCharArray()) |ForEach-Object {$_})
-Write-Host "Random Password: $Random"
