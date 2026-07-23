6e6fc7baa1b5655abfae52adfe9751574d29a228
QuimariCurry <quimari.curry@gmail.com>
2026-07-22 20:05:35 -0400

    Add files via upload

---
+++ GenerateRandomPassword/GenerateRandomPassword.ps1
@@ -0,0 +1,7 @@
+﻿param(
+    [int]$length = 14
+)
+$character = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()'
+$characterarray = $character.ToCharArray()
+$random = -join (1..$length |ForEach-Object{$characterarray | Get-Random})
+Write-Host "Random Password: $Random"
